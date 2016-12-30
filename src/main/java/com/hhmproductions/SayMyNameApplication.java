ce41d6d93fb601225b5e3c41ca36df71eead9f0d
Hesham Massoud <hesham.massoud@commercetools.de>
2016-12-30 16:45:42 +0100

    initial commit.

---
+++ src/main/java/com/hhmproductions/SayMyNameApplication.java
@@ -0,0 +1,12 @@
+package com.hhmproductions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayMyNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayMyNameApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hhmproductions/SayMyNameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hhmproductions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SayMyNameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
