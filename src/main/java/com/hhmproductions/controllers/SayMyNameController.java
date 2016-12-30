70255fa191cd74a9c7ac1b0c85bc2f3931d6bb15
Hesham Massoud <hesham.massoud@commercetools.de>
2016-12-30 17:06:35 +0100

    Add say endpoint.

---
+++ src/main/java/com/hhmproductions/controllers/SayMyNameController.java
@@ -0,0 +1,42 @@
+package com.hhmproductions.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by heshammassoud on 30/12/16.
+ */
+@RestController
+@RequestMapping("/say")
+public class SayMyNameController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/{name}")
+    public String index(@PathVariable String name) {
+        return "Hi " + name +  "! :)";
+    }
+
+    @RequestMapping("/**")
+    public String getAll() throws NoSuchMethodException {
+        logger.error("This endpoint is not found!");
+        if (true)
+            throw new NoSuchMethodException();
+        return "Get all resources";
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class, NullPointerException.class})
+    void handleBadRequests(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.BAD_REQUEST.value(), "Please enter the correct value!");
+    }
+
+    @ExceptionHandler({NoSuchMethodException.class})
+    void handleNotFoundRequests(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value(), "This page is not available!");
+    }
+
+}
